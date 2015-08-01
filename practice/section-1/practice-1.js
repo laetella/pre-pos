383c1e996adc8296d701b9eb9e453ef2b1ebed17
laetella <laetella@outlook.com>
2015-08-01 09:20:56 +0800

    first commit

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,3 +1,13 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var result = [];
+  var num = 0;
+  for(var elem_a=0; elem_a<collection_a.length; elem_a++) {
+    for(var elem_b=0; elem_b<collection_b.length; elem_b++) {
+      if(collection_a[elem_a] === collection_b[elem_b]) {
+        result[num++] = collection_a[elem_a];
+      }
+    }
+  }
+  return result;
 }
